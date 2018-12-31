f14e03d6aa3da495604600f229e308dc93264beb
handiko <handikogesang@gmail.com>
2018-12-31 18:07:33 +0700

    Adding Random String

---
+++ Arduino-Sketches/Test/AFSK_From_String/AFSK_From_String.ino
@@ -0,0 +1,200 @@
+/*
+ *  Random Two Tones Test - Test code to make your Arduino UNO to outputs
+ *  1200 Hz and 2400 Hz square wave signal switching from one into another
+ *  randomly
+ *  
+ *  Copyright (C) 2018 - Handiko Gesang - www.github.com/handiko
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <math.h>
+
+// Defines the Square Wave Output Pin
+#define OUT_PIN 2
+
+#define _1200   1
+#define _2400   0
+
+bool nada = _2400;
+
+/*
+ * SQUARE WAVE SIGNAL GENERATION
+ * 
+ * baud_adj lets you to adjust or fine tune overall baud rate
+ * by simultaneously adjust the 1200 Hz and 2400 Hz tone,
+ * so that both tone would scales synchronously.
+ * adj_1200 determined the 1200 hz tone adjustment.
+ * tc1200 is the half of the 1200 Hz signal periods.
+ * 
+ *      -------------------------                           -------
+ *     |                         |                         |
+ *     |                         |                         |
+ *     |                         |                         |
+ * ----                           -------------------------
+ * 
+ *     |<------ tc1200 --------->|<------ tc1200 --------->|
+ *     
+ * adj_2400 determined the 2400 hz tone adjustment.
+ * tc2400 is the half of the 2400 Hz signal periods.
+ * 
+ *      ------------              ------------              -------
+ *     |            |            |            |            |
+ *     |            |            |            |            |            
+ *     |            |            |            |            |
+ * ----              ------------              ------------
+ * 
+ *     |<--tc1200-->|<--tc1200-->|<--tc1200-->|<--tc1200-->|
+ *     
+ */
+const float baud_adj = 0.86;
+const float adj_1200 = 1.0 * baud_adj;
+const float adj_2400 = 1.0 * baud_adj;
+unsigned int tc1200 = (unsigned int)(0.5 * adj_1200 * 1000000.0 / 1200.0);
+unsigned int tc2400 = (unsigned int)(0.5 * adj_2400 * 1000000.0 / 2400.0);
+
+/*
+ * This string will be randomly chopped off and be used to generate AFSK
+ * signals, over and over again. This constant is stored in the program memory
+ * to save RAM.
+ */
+PROGMEM const char strings[860]={
+"b0NWtAXLKj0Sn8WRsakzQS8JN25zAAf3md5ILaYty6jvZHrq1QU1CWfC6tKOMY7cFCopla9sn0bn26zcd9qRHFWflqMcmMwx9ZDmzxrs4cfjiMox4R0pNCB0fm26gDVc\
+dMCZcVOnovLDWUlFHL0m2ULj3SVJonE4swIlemv2miVFJ3hjETh54cubpJhefhHtOGlwwtd64PigxsjzB3oXI6tJR3sCd84sheQis2DrnBZPd4pYdZvv6nx01hDeQNiU\
+YGilAHb7cdqlEIMwhHVaqIgn43JOwQzSMGOWvAbFdSxLyoUd8rYeyVWHxW3tyJS7wjWjsr1UV3RCkPBL4XhMpceV3z0zu6y9rQGWxBwVAbBliOo630lkdmwRkuMB0INN\
+cS4CjELYzsVQnEnX5OMCryDdbFEGwCpDEiFPETlP4EeqsYI6ACIRsM9A8buf1eecrwBKgkT3Ty0mHlOjc4ibBiJCJB5vTzvEbQdfgsLGubfPL1Y8Vb5PAzwCGVotWxUP\
+UPamGgBezXZ4JbOAbUfXGEM1ppuRtam8zk4ePExs1ccD4qumNt0pvfEWyCiIrVuLAK1TGoOG9rE0U0wCaLILlmLiTu1UtPMSTm1sZzEAdunENMmMrHH4bO5W3dL36Njo\
+q7fCVyFGiIurYBcmamYRWHFas3f6DCN7IpOiKo0PM1EIf7eeVegEB4lQZ5EVSXJ4HpGodk4h903bu4KIfm2VilJUUtjiy9lMqTXGliafDss5zBGpL8S7yh1z2NdgD8Tr\
+RGXR4EJ9gSiJTCBiGoSe1uzoeqPNV1pMM7ld7bKbTriOlBNyTCm7lx7cM8J5IsO4iegCSjG0OzwiQEhed7hvS2b78Qu"
+};
+
+/*
+ * 
+ */
+void set_nada_1200(void);
+void set_nada_2400(void);
+void set_nada(bool nada);
+
+void send_char_NRZI(unsigned char in_byte);
+void send_string(const char *in_string);
+
+void set_io(void);
+void print_code_version(void);
+
+/*
+ * 
+ */
+void set_nada_1200(void)
+{
+  digitalWrite(OUT_PIN, HIGH);
+  delayMicroseconds(tc1200);
+  digitalWrite(OUT_PIN, LOW);
+  delayMicroseconds(tc1200);
+}
+
+void set_nada_2400(void)
+{
+  digitalWrite(OUT_PIN, HIGH);
+  delayMicroseconds(tc2400);
+  digitalWrite(OUT_PIN, LOW);
+  delayMicroseconds(tc2400);
+  
+  digitalWrite(OUT_PIN, HIGH);
+  delayMicroseconds(tc2400);
+  digitalWrite(OUT_PIN, LOW);
+  delayMicroseconds(tc2400);
+}
+
+void set_nada(bool nada)
+{
+  if(nada)
+    set_nada_1200();
+  else
+    set_nada_2400();
+}
+
+/*
+ * This function will send one byte input and convert it
+ * into AFSK signal one bit at a time LSB first.
+ * 
+ * The encode which used is NRZI (Non Return to Zero, Inverted)
+ * bit 1 : transmitted as no change in tone
+ * bit 0 : transmitted as change in tone
+ */
+void send_char_NRZI(unsigned char in_byte)
+{
+  bool bits;
+  
+  for(int i = 0; i < 8; i++)
+  {
+    bits = in_byte & 0x01;
+
+    if(bits)
+    {
+      set_nada(nada);
+    }
+    else
+    {
+      nada ^= 1;
+      set_nada(nada);
+    }
+
+    in_byte >>= 1;
+  }
+}
+
+void send_string(const char *in_string)
+{
+  int len = strlen(in_string);
+  
+  for(int j=0; j<len; j++)
+  {
+    send_char_NRZI(in_string[j]);
+  }
+}
+
+/*
+ * 
+ */
+void set_io(void)
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(OUT_PIN, OUTPUT);
+
+  Serial.begin(115200);
+}
+
+void print_code_version(void)
+{
+  Serial.println(" ");
+  Serial.print("Sketch:   ");   Serial.println(__FILE__);
+  Serial.print("Uploaded: ");   Serial.println(__DATE__);
+  Serial.println(" ");
+  
+  Serial.println("Random Two Tones Test 1200 Hz & 2400 Hz - Started");
+}
+
+/*
+ * 
+ */
+
+void setup()
+{
+  set_io();
+  print_code_version();
+}
+
+void loop()
+{
+  send_string(strings);
+}
